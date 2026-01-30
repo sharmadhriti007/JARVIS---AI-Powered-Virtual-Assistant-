681ce2c6e95ee4dcd0a4110c5798d55f79b1da6d
Dhriti sharma <sharmadhriti1751@gmail.com>
2026-01-30 19:53:11 +0530

    Add files via upload

---
+++ jarvis-personal-ai-virtual-assistance-main/musicLibrary.py
@@ -0,0 +1,10 @@
+music = {
+    "lalkara": "https://youtu.be/-mgGnx1p3b8?si=0A8c8oILBE35v9Z4",
+    "kun": "https://youtu.be/T94PHkuydcw?si=BQSUrHuZtyVmVQKI",
+    "wouldn't": "https://youtu.be/sZ548uhFtNU?si=Zb1kkpd99cKSBvhD",
+    "rightnow": "https://youtu.be/vIaH35-MLsk?si=z8wlAKmH_L3b6Sjb "
+
+
+}
+
+    
